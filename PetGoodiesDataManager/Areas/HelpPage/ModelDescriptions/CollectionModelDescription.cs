497105bb999fb751d35e215041040801c4968971
Chane Du Toit <chanedt1008@gmail.com>
2021-12-27 20:55:24 +0200

    Add new Web API Project
    - Adds a blank Web API project with updated nuget packages

---
+++ PetGoodiesDataManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PetGoodiesDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ PetGoodiesDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PetGoodiesDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
